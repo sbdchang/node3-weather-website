c99f09ff71a833cde7983dc5a00e50ce6d73ff59
Steven Chang <sbdchang@design.upenn.edu>
2020-11-17 23:03:04 -0500

    Add more information to forecast

--- src/Utils/forecast.js
+++ src/Utils/forecast.js
@@ -11,8 +11,10 @@ const forecast = (latitude, longitude, callback) => {
         } else if (response.body.error) {
             callback("Unable to get weather for this location.", undefined);
         } else {
-            callback(undefined, response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + 
-                                " degrees out. It feels like " + response.body.current.feelslike + " degrees out.");
+            callback(undefined, response.body.current.weather_descriptions[0] + ", with " + response.body.current.wind_speed +
+            " mph winds coming from the " + response.body.current.wind_dir + ". The air temperature is " + 
+            response.body.current.temperature + " degrees, and it feels like " + response.body.current.feelslike + 
+            " degrees.");
         }
     })                
 }
